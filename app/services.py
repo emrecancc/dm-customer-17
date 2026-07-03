80b8aac71b28d0d0fa6bdcae674acf96f470732c
emrecancc <emrecancerli55@gmail.com>
2026-07-03 03:39:04 +0300

    ci: introduce syn_py (s19)

--- app/services.py
+++ app/services.py
@@ -1,1 +1,6 @@
-# syntax fixed
+class UserService:
+    def __init__(self, db):
+        self.db = db
+
+    def get_user(self, user_id)
+        return self.db.query('SELECT * FROM users WHERE id = ?', user_id)
